63a1e56aa0d3d2c2851636e3b71d380cbaf7c045
kky1225 <kky971225@naver.com>
2023-06-15 21:38:07 +0900

    코딩테스트 level 1 문제 68번 ~ 68번

---
+++ codingTest/src/level1/exam60_69/exam68.java
@@ -0,0 +1,82 @@
+package level1.exam60_69;
+
+/*
+ * 문제 이름 : 둘만의 암호
+ * 문제 설명 : 두 문자열 s와 skip, 그리고 자연수 index가 주어질 때, 다음 규칙에 따라 문자열을 만들려 합니다. 암호의 규칙은 다음과 같습니다.
+			- 문자열 s의 각 알파벳을 index만큼 뒤의 알파벳으로 바꿔줍니다.
+			- index만큼의 뒤의 알파벳이 z를 넘어갈 경우 다시 a로 돌아갑니다. 
+			- skip에 있는 알파벳은 제외하고 건너뜁니다.
+			예를 들어 s = "aukks", skip = "wbqd", index = 5일 때, a에서 5만큼 뒤에 있는 알파벳은 f지만 [b, c, d, e, f]에서 'b'와 'd'는 skip에 포함되므로 세지 않습니다. 따라서 'b', 'd'를 제외하고 'a'에서 5만큼 뒤에 있는 알파벳은 [c, e, f, g, h] 순서에 의해 'h'가 됩니다. 
+			나머지 "ukks" 또한 위 규칙대로 바꾸면 "appy"가 되며 결과는 "happy"가 됩니다.
+			두 문자열 s와 skip, 그리고 자연수 index가 매개변수로 주어질 때 위 규칙대로 s를 변환한 결과를 return하도록 solution 함수를 완성해주세요.
+			s	skip	index	result
+ * 입출력 예 : "aukks"	"wbqd"	5	"happy"
+ * 입출력 예 : 
+ * URL : https://school.programmers.co.kr/learn/courses/30/lessons/155652
+ * 
+ * */
+public class exam68 {
+
+	// 내 풀이
+	public String solution(String s, String skip, int index) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(char c : s.toCharArray()) {
+            int i = 0;
+            
+            while(i < index) {
+                c += 1;
+                
+                if(c > 'z') {
+                    c = 'a';
+                }
+                
+                if(skip.contains(String.valueOf(c))) {
+                    continue;
+                }
+                
+                i++;
+            }
+            
+            sb.append((char) c);
+        }
+        
+        return sb.toString();
+    }
+	
+	// 남의 풀이
+	/* public String solution(String s, String skip, int index) {
+        String answer = "";
+
+        for (char c : s.toCharArray()) {
+            for (int i = index; i > 0; i--) {
+                c++;
+                if (c > 122) c -= 26;
+
+                while (skip.contains(String.valueOf(c))) {
+                    c++;
+                    if (c > 122) c -= 26;
+                }
+            }
+            answer += c;
+        }
+        return answer;
+    } */
+
+	// 남의 풀이
+    /* public String solution(String s, String skip, int index) {
+        String answer = "";
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        int idx;
+        for(int i =0; i<skip.length();i++) {
+            alphabet = alphabet.replace(Character.toString(skip.charAt(i)), "");
+        }
+        for(int i =0; i<s.length();i++) {
+            idx=alphabet.indexOf(Character.toString(s.charAt(i)))+index;
+            answer+= Character.toString(alphabet.charAt(idx%alphabet.length()));
+        }
+
+        return answer;
+    } */
+	
+}
