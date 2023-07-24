52ae0a247d04934a6e7139049627fe0ffe974547
kky1225 <kky971225@naver.com>
2023-07-24 20:36:15 +0900

    코딩테스트 level 0(코딩 기초 트레이닝) 문제 99번 ~ 100번

---
+++ codingTest/src/level0_1/exam100_145/exam100.java
@@ -0,0 +1,22 @@
+package level0_1.exam100_145;
+
+/*
+ * 문제 이름 : 특수문자 출력하기
+ * 문제 설명 : 다음과 같이 출력하도록 코드를 작성해 주세요. !@#$%^&*(\'"<>?:;
+ * URL : https://school.programmers.co.kr/learn/courses/30/lessons/181948
+ * 
+ * */
+public class exam100 {
+
+	// 내 풀이
+	public static void main(String[] args) {
+        System.out.println("!@#$%^&*(\\\'\"<>?:;");
+    }
+	
+	// 남의 풀이
+	/* public static void main(String[] args) {
+	    System.out.print(
+            "!"+"@"+"#"+"$"+"%"+"^"+"&"+"*"+"("+"\\"+"\'"+"\""+"<"+">"+"?"+":"+";");
+	} */
+	
+}
