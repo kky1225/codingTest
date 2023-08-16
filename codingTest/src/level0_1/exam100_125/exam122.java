20a1e547ea573246a1f5e012970f16e0e99511c5
kky1225 <kky971225@naver.com>
2023-08-16 21:54:29 +0900

    코딩테스트 level 0(코딩 기초 트레이닝) 문제 122번 ~ 122번

---
+++ codingTest/src/level0_1/exam100_125/exam122.java
@@ -0,0 +1,25 @@
+package level0_1.exam100_125;
+
+import java.util.Scanner;
+
+/*
+ * 문제 이름 : 문자열 출력하기
+ * 문제 설명 : 문자열 str이 주어질 때, str을 출력하는 코드를 작성해 보세요.
+ * URL : https://school.programmers.co.kr/learn/courses/30/lessons/181952
+ * 
+ * */
+public class exam122 {
+
+	// 내 풀이
+	public class Solution {
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        String a = sc.next();
+	        
+	        sc.close();
+	        
+	        System.out.println(a);
+	    }
+	}
+	
+}
